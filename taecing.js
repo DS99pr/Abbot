78d586d43fa46534203c08aa142727936c86a70a
Ds <devteachpl@gmail.com>
2026-02-05 23:10:18 +0100

    Dokumentacja języku w staroangielskim
    I tak wiem ze to jest biednie napisane ale nie umiem tego jezyka oni jakos dziwnie gadali xddddddd

---
+++ taecing.js
@@ -0,0 +1,25 @@
+// ABBOT
+// Jest to język programowania w starodawnym angielskim. 
+// Obejmuje słownictwo z różnych epok.
+// Abbot nama thaere bec sceal beon: anging.abb.
+// Hit shal beon onlighted with Node.js
+
+/// GETRECEDNES THAES SPRAECES
+
+// [] - weorc
+// "" - scripture
+// ; - her geendeth weorc, and niwe weorc onginth (ne thearf)
+
+/// WEORC
+
+/* apert[]; - apert (scripture odde rim)
+   bisen: apert[5]; -> 5              */
+/* raedan[]; - raedan (tacen .abb)
+   bisen: raedan["anging.abb"];       */
+
+/// CYN
+
+// Scripture - String 
+// Rim - Number
+
+// Her sceal beon sum thing.
